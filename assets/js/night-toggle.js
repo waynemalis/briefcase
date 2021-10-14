431db3591fdd4b15f791f2a0a8cf65656e45f2b8
jexsuap <38709387+jexsuap@users.noreply.github.com>
2021-10-14 14:11:16 -0600

    5.1

---
+++ assets/js/night-toggle.js
@@ -0,0 +1,4 @@
+let night = document.querySelector('.feather-moon')
+night.onclick = function(){
+  list.className = 'feather-sun'
+}
--- assets/scripts/script-m.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-  $("#OpenM").click(function(){
-    $("#Desplegar").toggleClass("oculto");
-  });
-});
